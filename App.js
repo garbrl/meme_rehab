2f932d896f34192616cc02cbbf4e4ef6279fe56b
garbrl <gabe__f@hotmail.com>
2022-02-15 22:42:43 -0800

    add game useState and handle function

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './components/home.js';
 import { GetUsers } from './firebase/users.js';
 import { GetGame } from './firebase/games.js';
 
 console.log(GetUsers());
 console.log(GetGame("AAA"));
 
+const [game, setGame] = useState({});
+
+const handleGame = (newGame) => {
+  if(newGame.created_by) {
+    setGame(newGame);
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
