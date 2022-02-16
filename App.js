37b01400d3318a0294b3151e9597ba35e044a811
garbrl <gabe__f@hotmail.com>
2022-02-15 23:42:43 -0800

    call home component and pass game handler

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './components/home.js';
@@ -6,22 +6,29 @@ import { GetUsers } from './firebase/users.js';
 import { GetGame } from './firebase/games.js';
 
 console.log(GetUsers());
-console.log(GetGame("AAA"));
-
-const [game, setGame] = useState({});
-
-const handleGame = (newGame) => {
-  if(newGame.created_by) {
-    setGame(newGame);
-    return true;
-  } else {
-    return false;
-  }
-}
 
 export default function App() {
+  const [game, setGame] = useState({});
+
+  const handleGame = (code) => {
+    if (code === "") return false;
+    
+    GetGame(code).then(newGame => {
+      console.log("just waited", newGame);
+      if(newGame.created_by) {
+        setGame(newGame);
+        return true;
+      } else {
+        return false;
+      }
+    })
+  }
+
+  useEffect(() => console.log("game", game), [game]);
+
   return (
     <>
+      <Home game={game} handleGame={handleGame} />
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
