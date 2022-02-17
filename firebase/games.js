50d5f2c1f155c613323b1e23f62fa0c20de267cb
garbrl <gabe__f@hotmail.com>
2022-02-16 21:11:49 -0800

    convert game code to uppercase

--- firebase/games.js
+++ firebase/games.js
@@ -3,7 +3,7 @@ import { db } from './index.js';
 
 // Get a game from database
 export async function GetGame(code) {
-    const gameRef = doc(db, 'games', code);
+    const gameRef = doc(db, 'games', code.toUpperCase());
     const gameSnapshot = await getDoc(gameRef);
     if (gameSnapshot.exists()) {
         return gameSnapshot.data();
