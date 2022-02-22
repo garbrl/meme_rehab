ba379874048386bd4343997e68d04133bd5f0e5c
garbrl <gabe__f@hotmail.com>
2022-02-21 22:08:42 -0800

    enable environment variables

--- firebase/index.js
+++ firebase/index.js
@@ -1,10 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
+console.log("react key: ");
+console.log(process.env.REACT_APP_AUTHDOMAIN);
+
 const firebaseApp = initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: "meme-rehab-1",
+    projectId: process.env.REACT_APP_PROJECTID,
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
     appId: process.env.REACT_APP_APPID,
